bef52d6b1063466892be50940176c908688c81eb
rsvallen <137022121+rsvallen@users.noreply.github.com>
2024-12-26 19:33:53 -0600

    Update Headless_API.js

--- Headless_API.js
+++ Headless_API.js
@@ -1,9 +1,10 @@
 /*
- HaxBall @ 2024 - Mario Carbajal - All rights reserved.
- 8a626f82
-*/
+    HaxBall @ 2024 - Mario Carbajal - All rights reserved.
+    8a626f82
+    */
 "use strict";
-console.log("Custom loaded.");
+console.log("Custom loaded");
+
 (function (va) {
 	function ob() {
 		return q.Zb(this, "");
@@ -491,6 +492,7 @@ console.log("Custom loaded.");
 				sa = g("token", null),
 				qb = k("geo", null),
 				Da = E.Se;
+			console.log(B, Ea);
 			if (null != qb && ((Da = fa.Te(qb)), 3 < Da.ib.length))
 				throw r.s("Invalid country code");
 			let y = new Oa();
@@ -3488,7 +3490,13 @@ console.log("Custom loaded.");
 					(a = c = this.kd),
 					(c.x = -a.x),
 					(c.y = -a.y));
-			} else (a = this.F.a), (b = this.K.a), (c = a.x - b.x), (a = -(a.y - b.y)), (b = Math.sqrt(a * a + c * c)), (this.X = new I(a / b, c / b));
+			} else
+				(a = this.F.a),
+					(b = this.K.a),
+					(c = a.x - b.x),
+					(a = -(a.y - b.y)),
+					(b = Math.sqrt(a * a + c * c)),
+					(this.X = new I(a / b, c / b));
 		}
 	}
 	class Qa {
